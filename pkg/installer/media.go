davidcassany/elemental

commit 62693ea532ef22877ccd68cbcf6e49a5b9b68d95

Ensure extension names do not overlap when copied inside the installation media
Signed-off-by: David Cassany <dcassany@suse.com>

pkg/installer/media.go
@@ -640,12 +640,13 @@ func (i Media) addInstallationAssets(root string, d *deployment.Deployment) erro
 
 	if d.BootConfig != nil && len(d.BootConfig.InitrdExtensions) > 0 {
 		extensions := []string{}
-		for _, extension := range d.BootConfig.InitrdExtensions {
-			err = vfs.CopyFile(i.s.FS(), extension, installPath)
+		for j, extension := range d.BootConfig.InitrdExtensions {
+			extFile := fmt.Sprintf("%d-%s", j, filepath.Base(extension))
+			err = vfs.CopyFile(i.s.FS(), extension, filepath.Join(installPath, extFile))
 			if err != nil {
 				return fmt.Errorf("copying initrd extension %q: %w", extension, err)
 			}
-			extensions = append(extensions, filepath.Join(LiveMountPoint, installDir, filepath.Base(extension)))
+			extensions = append(extensions, filepath.Join(LiveMountPoint, installDir, extFile))
 		}
 		d.BootConfig.InitrdExtensions = extensions
 	}

commit e206e8df98311b05b0051bc8302a81b35eadc1a7

Fix initrd extension regression
This commit fixes a regression introduced in 62693ea. The initrd
extension files should only be renamed in case of overlap, otherwise
the files are renamed twice at customize time and at install time
when feeding the recovery partition. The second rename is causing
a failure as at this stage the installation assests are not
expected to be changed anymore as they are already referenced in
the install.yaml.

Signed-off-by: David Cassany <dcassany@suse.com>

pkg/installer/media.go
@@ -639,9 +639,13 @@ func (i Media) addInstallationAssets(root string, d *deployment.Deployment) erro
 	}
 
 	if d.BootConfig != nil && len(d.BootConfig.InitrdExtensions) > 0 {
+		var extFile string
 		extensions := []string{}
 		for j, extension := range d.BootConfig.InitrdExtensions {
-			extFile := fmt.Sprintf("%d-%s", j, filepath.Base(extension))
+			extFile = filepath.Base(extension)
+			if ok, _ := vfs.Exists(i.s.FS(), filepath.Join(installPath, extFile)); ok {
+				extFile = fmt.Sprintf("%d_%s", j, extFile)
+			}
 			err = vfs.CopyFile(i.s.FS(), extension, filepath.Join(installPath, extFile))
 			if err != nil {
 				return fmt.Errorf("copying initrd extension %q: %w", extension, err)